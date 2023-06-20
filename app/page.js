8537b0dc484d1149bddc07b7eda6b6545f92bcf6
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2023-06-20 13:38:19 +0200

    FIRST PUSH

--- app/page.js
+++ app/page.js
@@ -1,95 +1,141 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Plx from "react-plx";
+import styles from "../styles/page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import bg from "/public/images/bg.png";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div style={{ height: "200vh" }}>
+        <Plx
+          parallaxData={[
+            {
+              start: 0,
+              end: 700,
+              easing: "ease-in",
+              properties: [
+                {
+                  startValue: 1,
+                  endValue: 2.1,
+                  property: "scale",
+                },
+              ],
+            },
+          ]}
+          style={{
+            position: "fixed",
+            left: 0,
+            top: 0,
+            width: "100%",
+            height: "100vh",
+            zIndex: 1000,
+          }}
+        >
+          <Image style={{ width: "100%" }} src={bg} alt="foreground" />
+        </Plx>
+        <Plx
+          parallaxData={[
+            {
+              start: 0,
+              end: 800,
+              properties: [
+                {
+                  startValue: "#222222",
+                  endValue: "#f4fcfb",
+                  property: "backgroundColor",
+                },
+              ],
+            },
+          ]}
+          style={{
+            position: "fixed",
+            left: 0,
+            top: 0,
+            width: "100%",
+            height: "100vh",
+          }}
+        >
+          <div style={{ width: "100vw", height: "100vh" }}></div>
+        </Plx>
+        <Plx
+          parallaxData={[
+            {
+              start: 0,
+              end: 400,
+              properties: [
+                {
+                  startValue: 1,
+                  endValue: 1.5,
+                  property: "scale",
+                },
+                {
+                  startValue: 1,
+                  endValue: 0,
+                  property: "opacity",
+                },
+              ],
+            },
+          ]}
+          style={{
+            position: "fixed",
+            left: 0,
+            top: "26vw",
+            width: "100%",
+            height: "100vh",
+            color: "#fff",
+          }}
+        >
+          <h1 className={styles.heading}>Lourdes Reguera</h1>
+        </Plx>
+        <Plx
+          parallaxData={[
+            {
+              start: 0,
+              end: 400,
+              properties: [
+                {
+                  startValue: 0,
+                  endValue: 1,
+                  property: "opacity",
+                },
+              ],
+            },
+          ]}
+          style={{
+            position: "fixed",
+            left: 0,
+            top: "30vw",
+            width: "100%",
+            height: "100vh",
+            zIndex: "3000",
+          }}
+        >
+          <Link href="/about">Portfolio</Link>
+        </Plx>
+        {/* <div
+        style={{
+          position: "fixed",
+          lefft: 0,
+          top: 0,
+          zIndex: 200,
+          paddingTop: "56%",
+          height: "400vh",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            background: "#000",
+            height: "100%",
+          }}
+        ></div>
+      </div> */}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section>
+        <p>hola</p>
+      </section>
     </main>
-  )
+  );
 }
