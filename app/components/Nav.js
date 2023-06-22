cb4b8c13e5aa7da5a80c08089ba9456271d9bdf1
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2023-06-22 13:09:37 +0200

    FEAT: add menu

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,20 +1,11 @@
 import Link from "next/link";
 import styles from "../../styles/nav.module.css";
+import DropdownMenu from "./DropdownMenu";
 
 export default function Nav() {
   return (
-    <nav className={styles.nav}>
-      <p className={styles.title}>
-        Løurdes Reguera <span className={styles.span}>6</span>
-      </p>
-      <ul className={styles.links__container}>
-        <li className={styles.link}>
-          <Link href='/about'>Sobre mí</Link>
-        </li>
-        <li className={styles.link}>
-          <Link href='/contact'>Contacto</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <DropdownMenu />
+    </>
   );
 }
